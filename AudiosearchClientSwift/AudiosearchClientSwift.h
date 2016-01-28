506cdf3abf3b4ea0a5115dc1f122fb81e5708d6a
Anders Howerton <anders.e.howerton@gmail.com>
2016-01-28 14:31:09 -0800

    Initial Commit

---
+++ AudiosearchClientSwift/AudiosearchClientSwift.h
@@ -0,0 +1,19 @@
+//
+//  AudiosearchClientSwift.h
+//  AudiosearchClientSwift
+//
+//  Created by Anders Howerton on 1/28/16.
+//  Copyright © 2016 Pop Up Archive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AudiosearchClientSwift.
+FOUNDATION_EXPORT double AudiosearchClientSwiftVersionNumber;
+
+//! Project version string for AudiosearchClientSwift.
+FOUNDATION_EXPORT const unsigned char AudiosearchClientSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AudiosearchClientSwift/PublicHeader.h>
+
+
